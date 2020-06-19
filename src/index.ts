afd4b9c79f423a039b388c2cc594616214eb017e
Adrian Vela <u201416198@upc.edu.pe>
2020-06-19 14:45:10 -0500

    Backend Unido

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,13 @@ import productoRoutes from "./routes/productoRoutes";
 //Importamos DetalleCarrito
 import detallecarritoRoutes from "./routes/detalleCarritoRoutes";
 
+//Estado,Conductor,Vendedor,Ubicacion,Forma de Pago y Orden
+import estadoRoutes from "./routes/estadoRoutes";
+import conductorRoutes from "./routes/conductorRoutes";
+import vendedorRoutes from "./routes/vendedorRoutes";
+import ubicacionRoutes from "./routes/ubicacionRoutes";
+import formaPagoRoutes from "./routes/formaPagoRoutes";
+import ordenRoutes from "./routes/ordenRoutes";
 //librerias
 import morgan from "morgan";
 import cors from "cors";
@@ -48,6 +55,12 @@ class Server {
     this.app.use("/api/categoria", categoriaRoutes);
     this.app.use("/api/producto", productoRoutes);
     this.app.use("/api/detalleCarrito", detallecarritoRoutes);
+    this.app.use("/api/estado", estadoRoutes);
+    this.app.use("/api/conductor", conductorRoutes);
+    this.app.use("/api/vendedor", vendedorRoutes);
+    this.app.use("/api/ubicacion", ubicacionRoutes);
+    this.app.use("/api/formaPago", formaPagoRoutes);
+    this.app.use("/api/orden", ordenRoutes);
   }
 
   start(): void {
---
+++ src/routes/ubicacionRoutes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import ubicacionController from "../controller/ubicacionController";
+
+class UbicacionRoutes {
+  public router: Router = Router();
+
+  constructor() {
+    this.config();
+  }
+  config(): void {
+    this.router.get("/", ubicacionController.list);
+    this.router.post("/", ubicacionController.create);
+    this.router.delete("/:id", ubicacionController.delete);
+    this.router.put("/:id", ubicacionController.put);
+  }
+}
+
+const ubicacionRoute = new UbicacionRoutes();
+export default ubicacionRoute.router;
