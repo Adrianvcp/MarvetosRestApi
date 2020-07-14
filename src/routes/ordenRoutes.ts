ef297b096d9ccb170d6f86a01696e7045b99ee08
haroldhuaccha2498@gmail.com <haroldhuaccha2498@gmail.com>
2020-07-14 10:59:19 -0500

    Historial usuario + interfaz admin

--- src/routes/ordenRoutes.ts
+++ src/routes/ordenRoutes.ts
@@ -11,6 +11,7 @@ class OrdenRoutes {
     this.router.get("/:id", ordenController.listD);
     this.router.get("/gid", ordenController.lastIdOrden);
     this.router.get("/vendedor/:id", ordenController.getOrdersxSeller);
+    this.router.get("/usuario/:id", ordenController.getOrdersxUser);
     this.router.post("/", ordenController.create);
     this.router.delete("/:id", ordenController.delete);
     //-----------------------------------------------
