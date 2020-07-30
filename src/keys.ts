92f0193e66ca4069ae0c95ca2b42c468967065b3
haroldhuaccha2498@gmail.com <haroldhuaccha2498@gmail.com>
2020-07-30 12:23:35 -0500

    gg

--- src/keys.ts
+++ src/keys.ts
@@ -1,9 +1,13 @@
 export default {
   database: {
-    host: "gator4163.hostgator.com",
-    user: "kato3445_marveto",
-    password: "marvetos@123",
-    database: "kato3445_marvetosDB",
+    // host: "gator4163.hostgator.com",
+    // user: "kato3445_marveto",
+    // password: "marvetos@123",
+    // database: "kato3445_marvetosDB",
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "marvetos6",
   },
 };
 //claves
