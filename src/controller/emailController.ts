cca70a956bf6ee4409ce38d49561ccf8cb84559b
Adrian Vela <u201416198@upc.edu.pe>
2020-08-05 10:44:54 -0500

    email

--- src/controller/emailController.ts
+++ src/controller/emailController.ts
@@ -7,28 +7,34 @@ import emailbody from "../email/emailbody";
 
 class EmailController {
   public async sendEmail(req: Request, res: Response): Promise<void> {
-    var data = req.body;
-    var transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "pedro.velacc@gmail.com",
-        pass: "72941202Acm",
-      },
-    });
-    const mailOptions = {
-      from: "pedro.velacc@gmail.com",
-      to: data.email,
-      subject: "Tu orden ha sido recibida exitosamente.",
-      html: emailbody.HtmlEmail(data.direccion, data.distrito, data.Orden),
-    };
-    transporter.sendMail(mailOptions, function (err, info) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(info);
-        res.json({ text: "Enviar Mensaje" });
-      }
-    });
+    try {
+      var data = req.body;
+      var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "pedro.velacc@gmail.com",
+          pass: "72941202#@#@Acm",
+        },
+      });
+      console.log(transporter);
+
+      const mailOptions = {
+        from: data.email,
+        to: ["pedro.velacc@gmail.com"],
+        subject: "Tu orden ha sido recibida exitosamente.",
+        html: emailbody.HtmlEmail(data.direccion, data.distrito, data.Orden),
+      };
+      transporter.sendMail(mailOptions, function (err, info) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(info);
+          res.json({ text: "Enviar Mensaje" });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   public async ticket(req: Request, res: Response): Promise<void> {
