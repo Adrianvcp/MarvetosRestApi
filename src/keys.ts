c636af5a8e2dd9d472844bce9f8683ecb5952e29
jesusChristian18 <chris.jesus1997@gmail.com>
2020-08-24 13:00:39 -0500

    cotrizacionV4

--- src/keys.ts
+++ src/keys.ts
@@ -1,18 +1,18 @@
-export default {
+/* export default {
   database: {
     host: "us-cdbr-east-02.cleardb.com",
     user: "bf6bbf3b266593",
     password: "91db2924",
     database: "heroku_0240a6dfc8668ba",
   },
-};
-
-//claves-asd
-/* export default {
-  database: {
-    host: "localhost",
-    user: "root",
-    password: "123456789",
-    database: "marvetosv9",
-  },
 }; */
+
+//credencial del hosting Nice
+export default {
+  database: {
+    host: "167.114.0.97",
+    user: "udrwxrda_marve",
+    password: "$r1)T[u4@oXS",
+    database: "udrwxrda_marvetosV1",
+  },
+};
