90d69e674cc53cf5df7ffe29c89c48550596a17c
jesusChristian18 <chris.jesus1997@gmail.com>
2020-08-28 16:06:54 -0500

    key

--- src/keys.ts
+++ src/keys.ts
@@ -1,19 +1,19 @@
-/* export default {
+export default {
   database: {
     host: "167.114.0.97",
     user: "udrwxrda_marve",
-    password: "$r1)T[u4@oXS",
+    password: "&!Tgr(-HP?of",
     database: "udrwxrda_marvetosV1",
   },
-}; */
+};
 
 //credencial del hosting Nice
 
-export default {
+/* export default {
   database: {
     host: "us-cdbr-east-02.cleardb.com",
     user: "bf6bbf3b266593",
     password: "91db2924",
     database: "heroku_0240a6dfc8668ba",
   },
-};
+}; */
